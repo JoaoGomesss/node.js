db057c8a701b2f18bb9c1aa211d88844dfb9f775
João Gomes <jowgomes3@gmail.com>
2024-06-20 08:51:51 -0300

    cap.5

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,19 +1,43 @@
 
 const express = require('express')
+, path = require('path')
 , load = require("express-load")
-// , routes = require('./routes/home')
-, app = express();
+, cookieParser = require('cookie-parser')
+, session = require('express-session')
+, bodyParser = require('body-parser')
+, methodOverride = require('method-override')
+, app = express()
+, error = require('./middleware/error');
 
-app.set('views', __dirname + '/views');
+// , routes = require('./routes/home')
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+
+app.use(cookieParser('ntalk'));
+app.use(session({
+  secret: 'ntalk',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+// app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // app.get('/', routes.index);
 // app.get('/usuarios', routes.user.index);
 
 load("models").then("controllers").then("routes").into(app)
 
+app.use(error.notFound);
+app.use(error.serverError);
+
+
 app.listen(3000, function(){
 console.log("Ntalk no ar.");
 })
+
--- ntalk/controllers/home.js
+++ ntalk/controllers/home.js
@@ -2,7 +2,24 @@ module.exports = function(app) {
     let HomeController = {
     index: function(req, res) {
     res.render('home/index');
-    }
+    },
+    login: function(req, res) {
+        let email = req.body.usuario.email
+        , nome = req.body.usuario.nome;
+        if(email && nome) {
+        let usuario = req.body.usuario;
+        usuario['contatos'] = [];
+        req.session.usuario = usuario;
+        res.redirect('/contatos');
+        } else {
+        res.redirect('/');
+        }
+        },
+        logout: function(req, res) {
+            req.session.destroy();
+        res.redirect('/');
+        }
+
     };
     return HomeController;
     };
